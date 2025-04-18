8ccd46cac558d08be02f3539cf61d5fe300f8e04
NoMercyPeace <ciaoromankuba@gmail.com>
2025-04-18 19:10:28 +0200

    Plugin version: 1.0 development state: 0.1 additions: small to none additions

---
+++ src/main/java/me/mercy/meInventories/MeInventories.java
@@ -0,0 +1,31 @@
+package me.mercy.meInventories;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MeInventories extends JavaPlugin {
+
+    private static MeInventories main;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        main = this;
+
+        /*
+        Configuration logic, when the configuration doesn't exist, or its blank,
+        it creates the default configuration file
+        */
+
+        if (getConfig().getKeys(true).isEmpty()) {
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static MeInventories getMain(){return main;}
+}
