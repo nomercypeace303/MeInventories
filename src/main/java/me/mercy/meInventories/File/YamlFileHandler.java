f89a8b7fb84447ccfb4b06fa3815d837db1ba782
NoMercyPeace <ciaoromankuba@gmail.com>
2025-04-19 01:09:32 +0200

    Plugin version: 1.0 development state: 0.3 additions: QoF changes for easier use

--- src/main/java/me/mercy/meInventories/File/YamlFileHandler.java
+++ src/main/java/me/mercy/meInventories/File/YamlFileHandler.java
@@ -1,7 +1,9 @@
 package me.mercy.meInventories.File;
 
+import me.mercy.meInventories.MeInventories;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,41 +12,59 @@ public class YamlFileHandler {
 
     private final File file;
     private final FileConfiguration config;
+    private final Plugin plugin = MeInventories.getMain();
 
+    //------------------------------------------------------------------------------------------------------------------
+    /*
+        Costruttore
+    */
+    //------------------------------------------------------------------------------------------------------------------
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    public YamlFileHandler(String filePath) throws IOException {
+    public YamlFileHandler(String filePath) {
         this.file = new File(filePath);
 
-        // Crea file e cartelle se non esistono
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
+        try {
+            // Crea file e cartelle se non esistono
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            plugin.getLogger().severe("Errore nella creazione del file: " + filePath);
+            plugin.getLogger().severe(e.toString());
         }
 
         // Carica il file in memoria
         this.config = YamlConfiguration.loadConfiguration(file);
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     /*
         Tutti i metodi per il file
     */
-
+    //------------------------------------------------------------------------------------------------------------------
     public void set(String path, Object value) {
         config.set(path, value);
     }
 
-    public void save() throws IOException {
-        config.save(file);
+    public void save() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            plugin.getLogger().severe("Errore nel salvataggio di: " + file.getName());
+            plugin.getLogger().severe(e.toString());
+        }
     }
 
     public boolean contains(String path) {
         return config.contains(path);
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     /*
         Tutti i getters
     */
-
+    //------------------------------------------------------------------------------------------------------------------
     public Object get(String path) {return config.get(path);}
     public String getString(String path) {return config.getString(path);}
     public int getInt(String path) {return config.getInt(path);}
--- src/main/java/me/mercy/meInventories/MeInventories.java
+++ src/main/java/me/mercy/meInventories/MeInventories.java
@@ -1,5 +1,6 @@
 package me.mercy.meInventories;
 
+import me.mercy.meInventories.File.YamlFileHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MeInventories extends JavaPlugin {
@@ -15,18 +16,32 @@ public final class MeInventories extends JavaPlugin {
             Configuration logic, when the configuration doesn't exist, or its blank,
             it creates the default configuration file
         */
-
         if (getConfig().getKeys(true).isEmpty()) {
             getConfig().options().copyDefaults(true);
             saveConfig();
+        }
+
+        /*
+            First start logic, when the plugin starts for the first time
+            it creates the default inventory file
+        */
+        if (getConfig().getBoolean("PluginInfo.FirstStart", true)) {
+            YamlFileHandler firstStartFile = new YamlFileHandler("plugin/MeInvetories/invetory.yml");
+            firstStartFile.set("Inventory.Slots.1.Item.Type", "DIAMOND_SWORD");
+            getConfig().set("PluginInfo.FirstStart", false);
         }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+
+
     }
 
-    public static MeInventories getMain(){return main;}
+    public static MeInventories getMain() {
+        return main;
+    }
 
 }
